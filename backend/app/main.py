69fe047e3692a9c7f08d6a7efdb7505b3c377574
rootless <ripon1995dc@gmail.com>
2026-02-10 19:29:55 +0600

    request logger setup (#2)

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.router import api_router
 from app.core.exception_handlers import register_exception_handlers
-from app.core.log_config import setup_stdlib_colored_logging
+from app.core.log_config import configure_logging
+from app.middleware.request_logger import RequestLoggerMiddleware
+
+# step logger
+configure_logging(level=settings.LOG_LEVEL, environment=settings.ENVIRONMENT)
 
 app = FastAPI(title='Grocery Helper APIs')
 
-setup_stdlib_colored_logging(level=settings.LOG_LEVEL, for_dev=settings.DEBUG)
+# request logger
+app.add_middleware(RequestLoggerMiddleware, env_name=settings.ENVIRONMENT)
 
 register_exception_handlers(app)
 
