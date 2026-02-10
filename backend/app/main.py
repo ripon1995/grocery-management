ebbc2fa7ca0c94b765f886e4d2e184e67bab5d25
Ripon <alamin.ripon@brainstation-23.com>
2026-02-10 21:28:58 +0600

    cors issue fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.router import api_router
 from app.core.exception_handlers import register_exception_handlers
@@ -9,6 +10,19 @@ from app.middleware.request_logger import RequestLoggerMiddleware
 configure_logging(level=settings.LOG_LEVEL, environment=settings.ENVIRONMENT)
 
 app = FastAPI(title='Grocery Helper APIs')
+
+origins = [
+    "http://localhost:5173",
+]
+
+# 2. Add the Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Which domains can talk to the API
+    allow_credentials=True,  # Allow cookies/auth headers
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # request logger
 app.add_middleware(RequestLoggerMiddleware, env_name=settings.ENVIRONMENT)
