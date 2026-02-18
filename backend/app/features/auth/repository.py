f69994ce8830385e4d6cb3886dce4b38565830a9
Ripon <alamin.ripon@brainstation-23.com>
2026-02-18 10:28:41 +0600

    service and repository file created

---
+++ backend/app/features/auth/repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.features.auth.models import User
+
+
+class AuthRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_by_email(self, email: str):
+        stmt = select(User).where(User.email == email)
+        result = await self.session.execute(stmt)
+        return result.scalars().one_or_none()
+
+
