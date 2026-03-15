d3108daf088aacc00a8b736f435ad58669894e76
rootless <ripon1995dc@gmail.com>
2026-03-16 03:55:00 +0600

    cart icon changed

--- frontend/src/components/common/MonthlyGroceryAppBar.tsx
+++ frontend/src/components/common/MonthlyGroceryAppBar.tsx
@@ -1,13 +1,13 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import {Divider} from "@mui/material";
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBagOutlined';
+import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 
 function MonthlyGroceryAppBar() {
     return (
         <Box sx={{width: '100%', mt: 2, mb: 4}}>
             <Box sx={{display: 'flex', alignItems: 'center'}}>
-                <ShoppingBagIcon></ShoppingBagIcon>
+                <ShoppingCartSharpIcon fontSize={"large"}></ShoppingCartSharpIcon>
                 <Typography variant="h4" sx={{fontWeight: 'bold', letterSpacing: 1, fontFamily: 'ui-rounded'}}>
                     Monthly Grocery Manager
                 </Typography>
