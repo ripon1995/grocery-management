ab3ce09169431221973ec9562dc98ded5966b40f
rootless <ripon1995dc@gmail.com>
2026-03-16 02:26:37 +0600

    env_file path corrected

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 10
 
     model_config = SettingsConfigDict(
-        env_file='backend/.env',
+        env_file=('.env', 'backend/.env'),
         env_file_encoding='utf-8',
         case_sensitive=False,
         extra='ignore'
