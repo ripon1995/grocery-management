87386a03b6d2e09ebce56aa0c3d4b9bf523dfda0
rootless <ripon1995dc@gmail.com>
2026-03-23 02:55:35 +0600

    removed try catch block and added it into store

--- frontend/src/api/endpoints/GroceryApi.ts
+++ frontend/src/api/endpoints/GroceryApi.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import axiosInstance from "../axiosInstance.ts";
 import type {IGroceryListItem} from "../../types/IGroceryList.ts";
 import type {GroceryListResponse} from "../types/responses/GroceryListResponse.ts";
@@ -6,7 +5,6 @@ import API_ENDPOINT from "../../constants/apiEndpoints.ts";
 import type {IGroceryCreateItem} from "../types/requests/CreateGroceryItem.ts";
 import type {IGroceryDetail} from "../../types/IGroceryDetail.ts";
 import type {IGroceryDetailApiResponse} from "../types/responses/GroceryDetailResponse.ts";
-import {BaseError} from "../types/common.ts";
 import type {IPayloadGroceryItemUpdate} from "../types/requests/UpdateGroceryItem.ts";
 
 
@@ -15,7 +13,6 @@ export const getGroceries = async (): Promise<IGroceryListItem[]> => {
     const grocery_list: IGroceryListItem[] = response.data.map(item => ({
         ...item
     }));
-    console.log(`Fetched ${grocery_list.length} items`);
     return grocery_list;
 }
 
@@ -25,40 +22,16 @@ export const createGroceries = async (newItem: IGroceryCreateItem): Promise<void
 }
 
 export const getGroceryDetail = async (grocery_id: string): Promise<IGroceryDetail> => {
-    try {
-        const response = await axiosInstance.get<IGroceryDetailApiResponse>(API_ENDPOINT.GROCERY_DETAIL.replace(':id', grocery_id));
-        return response.data;
-    } catch (error) {
-        if (axios.isAxiosError(error) && error.response?.data) {
-            throw new BaseError(error.response.data);
-        }
-        // Returns the object with your default fallbacks
-        throw new BaseError();
-    }
+    const response = await axiosInstance.get<IGroceryDetailApiResponse>(API_ENDPOINT.GROCERY_DETAIL.replace(':id', grocery_id));
+    return response.data;
 }
 
 export const updateGrocery = async (grocery_id: string, payload: IPayloadGroceryItemUpdate): Promise<void> => {
-    try {
-        await axiosInstance.put<void>(API_ENDPOINT.GROCERY_UPDATE.replace(':id', grocery_id), payload);
-        return;
-    } catch (error) {
-        if (axios.isAxiosError(error) && error.response?.data) {
-            throw new BaseError(error.response.data);
-        }
-        // Returns the object with your default fallbacks
-        throw new BaseError();
-    }
+    await axiosInstance.put<void>(API_ENDPOINT.GROCERY_UPDATE.replace(':id', grocery_id), payload);
+    return;
 }
 
 export const deleteGrocery = async (grocery_id: string): Promise<void> => {
-    try {
-        await axiosInstance.delete<void>(API_ENDPOINT.GROCERY_DELETE.replace(':id', grocery_id));
-        return;
-    } catch (error) {
-        if (axios.isAxiosError(error) && error.response?.data) {
-            throw new BaseError(error.response.data);
-        }
-        // Returns the object with your default fallbacks
-        throw new BaseError();
-    }
+    await axiosInstance.delete<void>(API_ENDPOINT.GROCERY_DELETE.replace(':id', grocery_id));
+    return;
 }
