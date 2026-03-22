09f4f7c7c173710770527e86aaf2efadb798bac5
rootless <ripon1995dc@gmail.com>
2026-03-23 01:32:07 +0600

    basic logger setup with loglevel library

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import log from 'loglevel';
+
+
+log.setLevel(import.meta.env.DEV ? 'debug' : 'warn');
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -8,13 +12,38 @@ const axiosInstance = axios.create({
     },
 });
 
-// TODO => Add interceptors to handle error or auth token
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//         console.error('API Error:', error.response?.data || error.message);
-//         return Promise.reject(error);
-//     }
-// );
+
+axiosInstance.interceptors.request.use(
+    function (config) {
+        const token = localStorage.getItem('token');
+
+        // --- LOGGING WITH LOGLEVEL ---
+        log.info(`%c[API REQUEST] ${config.method?.toUpperCase()} -> ${config.url}`, "color: #00ff00; font-weight: bold;");
+        log.debug("Headers:", config.headers);
+        if (config.data) log.debug("Body:", config.data);
+        // ------------------------------
+        // TODO => Add interceptors to handle error or auth token
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        log.debug(response)
+        return response
+    },
+    (error) => {
+        log.debug('API Error:', error.response?.data || error.message);
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
