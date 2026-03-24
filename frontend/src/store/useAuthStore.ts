2a71c518d3fe69216dd6d3402aeb463467f5a886
Ripon <alamin.ripon@brainstation-23.com>
2026-03-24 20:12:47 +0600

    token added in axios header and stored in persist local storage of zustand

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,5 +1,6 @@
 import type {IUserLoginPayload} from "../api/types/requests/auth/UserLoginPayload.ts";
 import {create} from "zustand";
+import {persist, createJSONStorage} from 'zustand/middleware';
 import {login} from "../api/endpoints/AuthApi.ts";
 import log from "loglevel";
 import type {IToken} from "../types/IToken.ts";
@@ -15,26 +16,30 @@ interface IUserAuthState {
 }
 
 
-const useAuthStore = create<IUserAuthState>((set) => ({
-    token: null,
-    isLoading: false,
-    error: null,
-    login: async (payload: IUserLoginPayload) => {
-        try {
-            const tokenData = await login(payload);
-            set({
-                token: tokenData,
-                isLoading: false
-            });
-        } catch (err) {
-            if (err instanceof BaseError) {
-                set({error: err.message})
-            }
-            log.debug(`error getting here : ${err}`);
-            set({isLoading: false});
+const useAuthStore = create<IUserAuthState>()(
+    persist(
+        (set) => ({
+            token: null,
+            isLoading: false,
+            error: null,
+            login: async (payload: IUserLoginPayload) => {
+                set({isLoading: true, error: null}); // Start loading
+                try {
+                    const tokenData = await login(payload);
+                    set({token: tokenData, isLoading: false});
+                } catch (err) {
+                    if (err instanceof BaseError) set({error: err.message});
+                    log.debug(`error getting here : ${err}`);
+                    set({isLoading: false});
+                }
+            },
+            resetError: () => set({error: null})
+        }),
+        {
+            name: 'auth-storage', // Key name in localStorage
+            storage: createJSONStorage(() => localStorage),
         }
-    },
-    resetError: () => set({error: null})
-}));
+    )
+);
 
 export default useAuthStore;
