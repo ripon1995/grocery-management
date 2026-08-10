dab651a80ce46a918fce2b1efd529a3be59d99d1
Ripon <alamin.ripon@brainstation-23.com>
2026-08-11 00:13:26 +0600

    phase-2-rest-api-principles: add auth api versioning(FE updated)

--- frontend/src/constants/apiEndpoints.ts
+++ frontend/src/constants/apiEndpoints.ts
@@ -1,14 +1,18 @@
+const API_V1: string = '/api/v1';
+const GROCERIES: string = `${API_V1}/groceries`;
+const AUTH: string = `${API_V1}/auth`;
+
 const GROCERY_APIS = {
-    GROCERY_LIST: '/api/groceries/',
-    GROCERY_ADD: '/api/groceries/',
-    GROCERY_DETAIL: '/api/groceries/:id',
-    GROCERY_UPDATE: '/api/groceries/:id',
-    GROCERY_DELETE: '/api/groceries/:id',
-    GROCERY_BULK_SHOULD_INCLUDE: '/api/groceries/bulk/should-include',
+    GROCERY_LIST: `${GROCERIES}/`,
+    GROCERY_ADD: `${GROCERIES}/`,
+    GROCERY_DETAIL: `${GROCERIES}/:id`,
+    GROCERY_UPDATE: `${GROCERIES}/:id`,
+    GROCERY_DELETE: `${GROCERIES}/:id`,
+    GROCERY_BULK_SHOULD_INCLUDE: `${GROCERIES}/bulk/should-include`,
 }
 
 const AUTH_APIS = {
-    LOGIN: '/api/auth/login'
+    LOGIN: `${AUTH}/login`
 };
 
 
