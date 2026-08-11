e1806b4eef9db0f6d7d058d85e606971d6627db9
Ripon <alamin.ripon@brainstation-23.com>
2026-08-11 18:42:50 +0600

    topic-2-overload candidate

--- frontend/src/constants/utils.ts
+++ frontend/src/constants/utils.ts
@@ -1,9 +1,12 @@
-export const formatDate = (dateInput: string | Date | undefined) => {
-    if (!dateInput) return 'N/A';
+// overloaded method
+function formatDate(dateInput: Date): string;
+function formatDate(dateInput: string): string;
+function formatDate(dateInput: undefined): 'N/A';
 
-    return new Date(dateInput).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'short',
-        year: 'numeric',
-    });
-};
+// implementation signature
+function formatDate(dateInput: Date | string | undefined): string {
+    if (!dateInput) return 'N/A';
+    return new Date(dateInput).toLocaleDateString('en-US', {day: 'numeric', month: 'long', year: 'numeric'});
+}
+
+export { formatDate };
