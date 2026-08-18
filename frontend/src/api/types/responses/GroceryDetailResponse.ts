a1795103d2f16284aa6a3d922a95f8c9f2acb8a2
Ripon <alamin.ripon@brainstation-23.com>
2026-08-18 13:45:47 +0600

    day-7-ts: update readonly type

--- frontend/src/api/types/responses/GroceryDetailResponse.ts
+++ frontend/src/api/types/responses/GroceryDetailResponse.ts
@@ -1,19 +1,3 @@
-import type {GroceryStockStatus, GroceryType, Seller, GroceryCategory} from "../../../constants/enums.ts";
+import type {IGroceryDetail} from "../../../types/IGroceryDetail.ts";
 
-export interface IGroceryDetailApiResponse {
-    id: string;
-    name: string;
-    brand: string;
-    type: GroceryType;
-    current_price: number;
-    current_seller: Seller;
-    low_stock_threshold: number;
-    quantity_in_stock: number;
-    should_include: boolean;
-    category: GroceryCategory;
-    best_price: number;
-    best_seller: Seller;
-    stock_status: GroceryStockStatus;
-    created_at: Date,
-    updated_at: Date
-}
+export type IGroceryDetailApiResponse = Readonly<IGroceryDetail>
